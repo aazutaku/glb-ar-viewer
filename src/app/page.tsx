e722f77e7ab4846aa8e33c04a0159d424dd3a48d
azutaku <ymph6cb5jk@outlook.jp>
2025-06-10 21:30:16 +0900

    [update] テスト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,15 @@
 
 import { Canvas } from "@react-three/fiber";
 import { XR, createXRStore } from "@react-three/xr";
-import { Suspense, useEffect, useState } from "react";
-import { useGLTF } from "@react-three/drei";
-
-function Model() {
-  const { scene } = useGLTF("/models/ninja.glb");
-  scene.scale.set(20, 20, 20);
-  scene.position.set(0, 0, -10);
-  return <primitive object={scene} />;
-}
+import { useEffect, useState } from "react";
 
 const store = createXRStore();
 
 export default function Page() {
   const [enabled, setEnabled] = useState(false);
+  const [red, setRed] = useState(false);
 
+  // iOS向けのVariant SDKイベント対応
   useEffect(() => {
     const onLaunch = (e: Event) => {
       const customEvent = e as CustomEvent<{
@@ -25,12 +19,11 @@ export default function Page() {
       }>;
 
       if (customEvent.detail.launchRequired) {
-        window.location.href = customEvent.detail.launchUrl; // iOS対応用リダイレクト
+        window.location.href = customEvent.detail.launchUrl;
       }
     };
 
     window.addEventListener("vlaunch-initialized", onLaunch);
-
     return () => {
       window.removeEventListener("vlaunch-initialized", onLaunch);
     };
@@ -52,10 +45,17 @@ export default function Page() {
 
       <Canvas>
         <XR store={store}>
-          <ambientLight />
-          <Suspense fallback={null}>
-            <Model />
-          </Suspense>
+          <ambientLight intensity={1} />
+          <directionalLight position={[1, 2, 3]} />
+
+          <mesh
+            pointerEventsType={{ deny: "grab" }}
+            onClick={() => setRed(!red)}
+            position={[0, 1, -1]}
+          >
+            <boxGeometry />
+            <meshStandardMaterial color={red ? "red" : "blue"} />
+          </mesh>
         </XR>
       </Canvas>
     </div>
