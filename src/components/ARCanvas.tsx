8fca01c6e59d859e915dea48862e065d1e884ac2
azutaku <ymph6cb5jk@outlook.jp>
2025-06-29 23:56:27 +0900

    [動作確認] 2356

--- src/components/ARCanvas.tsx
+++ src/components/ARCanvas.tsx
@@ -213,7 +213,7 @@ export default function ARCanvas({ glbUrl, launcherURL, containerRef }: Props) {
                 </group>
               )}
 
-              <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1, -1]}>
+              <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, -1.5, -1]}>
                 <planeGeometry args={[0.5, 0.5]} />
                 <meshStandardMaterial color="gray" transparent opacity={0.5} />
               </mesh>
